b807160b5e04f6929e5a1222d7f8d984aa5b7025
carlosrafael6 <carlosrafael@yahoo.com>
2026-06-11 14:06:16 -0400

    Fix string formatting for title in streamlit_app.py
    Fix formatting of line 8

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(f"My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
