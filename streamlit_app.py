0bc472dcbcf674c1355624bdec8f0bec2de52c9a
carlosrafael6 <carlosrafael@yahoo.com>
2026-06-12 12:19:57 -0400

    Update data handling and display in Streamlit app
    Converted Snowpark DataFrame to Pandas DataFrame and updated data display logic.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # APP: Custom Smoothy Entry Form
 # Import python packages
 import streamlit as st
+import pandas as pd
 import requests
 from snowflake.snowpark.functions import col
 
@@ -19,8 +20,13 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:'
     , my_dataframe
@@ -30,6 +36,8 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:'
 if ingredients_list:
 
     ingredients_string = ''
+    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
